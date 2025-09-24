80de6362517b0d466eaa5d764b96dd93bb89bd1e
MD-Moniruzzaman-Arafat <arafatmonir091@gmail.com>
2025-09-24 22:57:41 +0600

    create auth context provider

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,13 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { RouterProvider } from 'react-router';
 import './index.css';
+import AuthProvider from './Provider/AuthProvider.jsx';
 import routes from './routes/Routes.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={routes} />
+    <AuthProvider>
+      <RouterProvider router={routes} />
+    </AuthProvider>
   </StrictMode>
 );
